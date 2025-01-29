ea843e8be09aa7f1ea329c0a9267bd3827c0aa95
Q4OS Team <q4os@q4os.org>
2025-01-29 19:30:19 +0100

    Rework scripts and framework to enable Debian style reporoducible build

--- 95_fix_symlinks.sh
+++ 95_fix_symlinks.sh
@@ -13,14 +13,28 @@ if [ ! -f "$ICON_DIR/index.theme" ] ; then
   exit 100
 fi
 
-#remove all the broken links
+#remove all the broken links, but try to fix svg vs png
 echo "Checking for broken symlinks ..."
 WKFL1="/tmp/.a_mlinks_1.lst" #list of icons symlinks
-find "$ICON_DIR/" -xtype l > $WKFL1
+find "$ICON_DIR/" -name '*.svg' -type l -o -name '*.png' -type l | xargs file | grep 'broken symbolic link to' | awk -F': ' '{ print $1 }' > $WKFL1
 LN_NUM1="$( wc -l < "$WKFL1" )"
 if [ "$LN_NUM1" != "0" ] ; then
   echo "  [W:] Found broken links: #$LN_NUM1, removing them ..."
-  rm -f $( cat "$WKFL1" )
+  while read LINE1 ; do
+    LN2SF="$( echo "$LINE1" | awk -F'.png$' '{ print $1 }' | awk -F'.svg$' '{ print $1 }' )"
+    if [ -e "$LN2SF.svg" ] ; then
+      echo -e "\nFixing symlink: $LN2SF.svg\n"
+      mv $LINE1 $LN2SF.svg
+    elif [ -e "$LN2SF.png" ] ; then
+      echo -e "\nFixing symlink: $LN2SF.png\n"
+      mv $LINE1 $LN2SF.png
+    else
+      printf '.'
+      rm $LINE1
+    fi
+  done < $WKFL1
+  printf '\n'
+#   rm -f $( cat "$WKFL1" )
 else
   echo "  No broken links found, Ok."
 fi
@@ -29,59 +43,16 @@ if [ "$FIX_DOUBLE_LINKS" != "1" ] ; then
   exit
 fi
 
-#check for double and multiple level symlinks
-echo "Checking for multiple level symlinks ..."
+echo "Determining all symlinks ..."
 WKFL2="/tmp/.a_mlinks_2.lst" #list of all symlinks in the icons direcotory
-WKFL3="/tmp/.a_mlinks_3.lst" #list of targets of symlinks with path resolved
-WKFL4="/tmp/.a_mlinks_4.lst" #list of target symlinks what point to another symlink
-WKFL5="/tmp/.a_mlinks_5.lst" #list of symlinks created as double symlinks bypass
+find "$ICON_DIR/" -name '*.svg' -type l -o -name '*.png' -type l | sort -u > $WKFL2
+echo "Number of all symlinks: #$(wc -l < "$WKFL2")"
 
-while [ "$SYMLINKS_OK" != "1" ] ; do
-  echo "  Starting new phase ..."
-  echo "  Determining symlinks ..."
-  find "$ICON_DIR/" -name '*.svg' -type l -ls | awk -F' ' '{ print $(NF-2)" -> "$NF }' > $WKFL2
-  find "$ICON_DIR/" -name '*.png' -type l -ls | awk -F' ' '{ print $(NF-2)" -> "$NF }' >> $WKFL2
-  # find "$ICON_DIR/" -name '*.svg' -type l -ls | awk -F' -> ' '{ print $(NF-1) }' | awk -F' ' '{ print $NF }' > $WKFL2
-  # find "$ICON_DIR/" -name '*.png' -type l -ls | awk -F' -> ' '{ print $(NF-1) }' | awk -F' ' '{ print $NF }' >> $WKFL2
-  echo "  Number of all symlinks: #$(wc -l < "$WKFL2")"
-
-  echo "  Resolving paths ..."
-  rm -f $WKFL3 ; touch $WKFL3
-  while read LINE1 ; do
-    HSTR1="$( echo "$LINE1" | awk -F' -> ' '{ print $1 }' | rev | cut -d'/' -f2- | rev )"
-    HSTR2="$( echo "$LINE1" | awk -F' ' '{ print $NF }' )"
-    echo "$LINE1 ( $HSTR1/$HSTR2 )" >> $WKFL3
-    # readlink -f "$LINE1" >> $WKFL3
-  done < $WKFL2
-
-  echo "  Determining double links ..."
-  rm -f $WKFL4 ; touch $WKFL4
-  while read LINE1 ; do
-    HSTR1="$( ls -l $( echo "$LINE1" | awk -F'(' '{ print $2 }' | awk -F' )' '{ print $1 }' ) )"
-    if [ "$( echo "$HSTR1" | grep ' -> ' )" ] ; then
-      echo "$LINE1 -> $( echo "$HSTR1" | awk -F' ' '{ print $NF }' )" >> $WKFL4
-    fi
-  done < $WKFL3
-
-  LN_NUM1="$( wc -l < "$WKFL4" )"
-  if [ "$LN_NUM1" != "0" ] ; then
-    echo "  [W:] Found double links: #$LN_NUM1, fixing them ..."
-    rm -f $WKFL5 ; touch $WKFL5
-    while read LINE1 ; do
-      LNK1="$( echo $LINE1 | awk -F' ' '{ print $1 }' )"
-      LNK2="$( echo $LINE1 | awk -F' ' '{ print $3 }' | rev | cut -d'/' -f2- | rev )"
-      LNK3="$( echo $LINE1 | awk -F' ' '{ print $8 }' )"
-      rm "$LNK1"
-      if [ -z "$( echo "$LNK2" | grep "/" )" ] ; then
-        LNK4="$LNK3"
-      else
-        LNK4="${LNK2}/${LNK3}"
-      fi
-      ln -s "${LNK4}" "$LNK1"
-      echo "$LNK1 -> $LNK4" >> $WKFL5
-    done < $WKFL4
-  else
-    echo "  No multiple level symlinks found, fix completed."
-    SYMLINKS_OK="1"
-  fi
-done
+echo "Fixing multiple level symlinks ..."
+while read LINE1 ; do
+  printf '.'
+  ABSPATH1="$( readlink -f "$LINE1" )"
+  # echo "$LINE1 -> $ABSPATH1"
+  rm "$LINE1"
+  ln -sr "$ABSPATH1" "$LINE1"
+done < $WKFL2
